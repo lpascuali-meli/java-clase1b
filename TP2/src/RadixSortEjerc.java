7dd68115001fff2ec025412d21e2c0de9ddba8bd
Lisandro Pascuali <lisandro.pascuali@mercadolibre.com>
2021-03-19 12:08:28 -0300

    Arreglos

--- TP2/src/RadixSortEjerc.java
+++ TP2/src/RadixSortEjerc.java
@@ -7,28 +7,26 @@ public class RadixSortEjerc
 {
 	public static void radixSort(int []arr)
 	{
-		// Creamos gestor de strings
-		StringUtil su = new StringUtil();
 		// Formateamos el arreglo original
-		String[] arrStr = su.toStringArray(arr);
-		su.lNormalize(arrStr, '0');
+		String[] arrStr = StringUtil.toStringArray(arr);
+		StringUtil.lNormalize(arrStr, '0');
 
 		// Creamos mapa para implementar algoritmo
-		SortedMap<Character, ArrayList<String>> treeMap = new TreeMap<Character, ArrayList<String>>();
+		SortedMap<Character, ArrayList<String>> treeMap = new TreeMap<>();
 		// Obtenemos la longitud de cada palabra
 		int longitud = arrStr[0].length();
 		for (int i = longitud - 1; i >= 0; i--) {
-			for (int j = 0; j < arrStr.length; j++) {
+			for (String value : arrStr) {
 				// Tomamos el char de la palabra
-				char c = arrStr[j].charAt(i);
+				char c = value.charAt(i);
 				// Consultamos si existe ese char en el diccionario
 				if (treeMap.get(c) != null) {
 					// Si existe agregamos al final de la lsita
-					treeMap.get(c).add(arrStr[j]);
+					treeMap.get(c).add(value);
 				} else {
 					// Si no existe creamos la lista y la asignamos a la key
 					treeMap.put(c, new ArrayList<String>());
-					treeMap.get(c).add(arrStr[j]);
+					treeMap.get(c).add(value);
 				}
 			}
 			// Restauramos arreglo original
@@ -38,18 +36,19 @@ public class RadixSortEjerc
 			for(Map.Entry<Character,ArrayList<String>> entry:treeMap.entrySet()) {
 				// Y en cada entrie recorremos la lista
 				ArrayList<String> listValue = entry.getValue();
-				for (int k = 0; k < listValue.size(); k++) {
+				for (String s : listValue) {
 					// Agregamos cada valor al arreglo
-					newArr[lastIndex] = listValue.get(k);
+					newArr[lastIndex] = s;
 					lastIndex++;
 				}
 			}
 			treeMap = new TreeMap<Character, ArrayList<String>>();
 			arrStr = newArr;
 		}
-		for(int i=0; i<arrStr.length;i++)
+		arr = StringUtil.toIntArray(arrStr);
+		for(int i=0; i < arr.length; i++)
 		{
-			System.out.print(arrStr[i]+(i<arrStr.length-1?",":""));
+			System.out.print(arr[i]+(i<arr.length-1?",":""));
 		}
 	}
 
